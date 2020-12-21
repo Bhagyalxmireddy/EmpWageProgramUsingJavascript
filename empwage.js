b7609dc6ad4d31f669f9cf5598acf5f5f6d9fe22
Bhagi <puppyreddy1997@gmail.com>
2020-12-21 22:11:16 +0530

    [Bhagyalaxmi]ADDED:Used Map to store the daily wage based on day

--- empwage.js
+++ empwage.js
@@ -24,16 +24,21 @@ function getWorkingHours(empCheck){
 let totalEmpHrs=0;
 let totalWorkingDays=0;
 let empDailyWageArray = new Array();
+let empDailyWageMap = new Map();
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empHrs =getWorkingHours(empCheck);
     totalEmpHrs+=empHrs;
     empDailyWageArray.push(CalculateWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays,CalculateWage(empHrs));
 
 }
 let empWage = CalculateWage(totalEmpHrs);
-console.log("Total Working Days:"+totalWorkingDays+" TotalHours:"+totalEmpHrs+" EmpWage:"+empWage); 
+console.log("UC6-Total Working Days:"+totalWorkingDays+" TotalHours:"+totalEmpHrs+" EmpWage:"+empWage);
+//UC8-storingDailyWage in Map
+console.log(empDailyWageMap);
+
 //UC7 Array healperFunction cal total wage using Array foreach traversal or reduce method 
 let totEmpWage = 0;
 function sum(dailyWage) {
