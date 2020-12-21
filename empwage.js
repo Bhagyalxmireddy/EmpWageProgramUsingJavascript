174c3ef6e947b4340a4ac799866de6a171b242d4
Bhagi <puppyreddy1997@gmail.com>
2020-12-21 18:44:41 +0530

    [Bhagyalaxmi]ADDED:Stroing The Values in array and retriving the Empwage

--- empwage.js
+++ empwage.js
@@ -4,6 +4,8 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 20;
 //UC3 using functions to get empHrs
 function getWorkingHours(empCheck){
     switch(empCheck){
@@ -15,18 +17,20 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
-let empHrs = 0;
-//UC4 calculating Empwage for a Month
-const NUM_OF_WORKING_DAYS = 10;
-//UC5 calculating Empwage To Reach No.Of WrkngDays or TotalWorkng Hrs
-const MAX_HRS_IN_MONTH = 100;
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
-    totalWorkingDays ++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+ //UC6 storing Daily Wage in array and retrive the empwage
+ function CalculateWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 - Total Days: " + totalWorkingDays +
-            ", Total Hrs: " +totalEmpHrs+ ", Emp Wage : " +empWage);
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyWageArray = new Array();
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs =getWorkingHours(empCheck);
+    totalEmpHrs+=empHrs;
+    empDailyWageArray.push(CalculateWage(empHrs));
+
+}
+let empWage = CalculateWage(totalEmpHrs) * WAGE_PER_HOUR;
+console.log("Total Working Days:"+totalWorkingDays+" TotalHours:"+totalEmpHrs+" EmpWage:"+empWage);           
