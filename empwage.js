50876bff7fd96c2185c59f0c0504298170faab3b
Bhagi <puppyreddy1997@gmail.com>
2020-12-21 16:33:17 +0530

    [Bhagyalaxmi]ADDED:Calculating The EmpWage To Reach No.of Working Days OR TotalWorkingDays

--- empwage.js
+++ empwage.js
@@ -17,10 +17,16 @@ function getWorkingHours(empCheck){
 }
 let empHrs = 0;
 //UC4 calculating Empwage for a Month
-const NUM_OF_WORKING_DAYS = 20;
-for(let day = 0 ; day < NUM_OF_WORKING_DAYS; day ++){
+const NUM_OF_WORKING_DAYS = 10;
+//UC5 calculating Empwage To Reach No.Of WrkngDays or TotalWorkng Hrs
+const MAX_HRS_IN_MONTH = 100;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays ++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total hrs: " +empHrs+ " EmpWage : " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 - Total Days: " + totalWorkingDays +
+            ", Total Hrs: " +totalEmpHrs+ ", Emp Wage : " +empWage);
